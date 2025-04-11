1ce03685bba301a6cab0c3e21d585468b45d039b
Mrjack1525 <kiransriya4@gmail.com>
2025-04-11 15:10:46 +0530

    First Commit

---
+++ Frontend/Medica/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <div>Medica - The Patient Scheduling</div><br/>
+      <div className='im'><img src='sample1.jpeg'></img></div>
+    </div>
+  )
+}
